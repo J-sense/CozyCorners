675df3ced82c3c3733e43a74cdc3797dd9c99abf
J-sense <121811557+J-sense@users.noreply.github.com>
2024-09-26 12:48:28 +0600

    tailwind configaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#E58411',
+        'secondary':'#1e1e1e',
+        'secondary-bg':'#f7f7f7',
+      },
+      fontFamily:{
+        'primary':'Poppins", sans-serif'
+      }
+    },
   },
   plugins: [],
 }

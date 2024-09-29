722e2bfba7a062c29899909b4aef31bb9b8ba1fa
J-sense <121811557+J-sense@users.noreply.github.com>
2024-09-29 23:47:23 +0600

    filltering

--- src/components/shared/Headline.jsx
+++ src/components/shared/Headline.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Headline = ({ children }) => {
   return (
-    <h1 className='font-bannerfont text-3xl font-bold my-6'>
+    <h1 className='font-bannerfont text-3xl font-bold py-6'>
       {children}
     </h1>
   )

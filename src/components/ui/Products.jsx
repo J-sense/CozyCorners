e15400c71a6ade3ff5d7894e1972ca732f144274
J-sense <121811557+J-sense@users.noreply.github.com>
2024-10-01 23:27:06 +0600

    selected category

--- src/components/ui/Products.jsx
+++ src/components/ui/Products.jsx
@@ -16,7 +16,7 @@ const Products = ({headline}) => {
         <div className='bg-[#EEEEEE] max-w-md mx-auto sm:rounded-full md:p-2 py-5 mb-16'> 
            <div className='flex flex-col sm:flex-row items-center md:justify-between sm:justify-center gap-4 duration-500 transition-all'>
             {categories.map((item,idx)=>(
-                <button onClick={()=>setCategoryfilter(item)} key={idx} className={`py-1.5 sm:px-5 px-8 hover:bg-primary hover:text-white transition-colors rounded-full`}>{item}</button>
+                <button onClick={()=>setCategoryfilter(item)} key={idx} className={`py-1.5 sm:px-5 px-8 hover:bg-primary hover:text-white transition-colors rounded-full ${categoryfilter===item ? 'bg-white text-primary' : 'text-secondary'}`}>{item}</button>
             ))}
            </div>
         </div>
